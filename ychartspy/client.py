147ea06a40094c2c34f50a12024d8110df4a4b06
hahnicity <grehm87@gmail.com>
2014-10-20 22:40:53 -0700

    Adding start and end date functionality

--- ychartspy/client.py
+++ ychartspy/client.py
@@ -1,8 +1,25 @@
+import re
+
 import requests
 
 from ychartspy.constants import (
     AVAILABLE_SECURITY_METRICS, YCHARTS_DATA_URL, YCHARTS_LOGIN_URL
 )
+
+
+def _validate_date_input(time_length, start_date, end_date):
+
+    if not time_length and not (start_date or end_date):
+        raise Exception(
+            "You must enter a time length to look from data back from the current time "
+            "or a start/end date to look for"
+        )
+    date_pattern = re.compile("(?:\d{2}\/){2}\d{4}")
+    err_msg = "Your {} date must be of the form mm/dd/yyyy yours was {}"
+    if start_date and not date_pattern.search(start_date):
+        raise Exception(err_msg.format("start", start_date))
+    if end_date and not date_pattern.search(end_date):
+        raise Exception(err_msg.format("end", end_date))
 
 
 class YChartsClient(object):
@@ -14,22 +31,31 @@ class YChartsClient(object):
     def _get_raw_data(self, response):
         return response.json()["chart_data"][0][0]["raw_data"]
 
-    def _make_request(self, ticker, metric, time_length, ticker_type):
-        params = self._set_request_params(ticker, metric, time_length, ticker_type)
+    def _make_request(self, ticker, metric, time_length, start_date, end_date, ticker_type):
+        params = self._set_request_params(
+            ticker, metric, time_length, start_date, end_date, ticker_type
+        )
         response = self.session.get(YCHARTS_DATA_URL, params=params)
         self._validate_response(response)
         return response
 
-    def _set_request_params(self, ticker, metric, time_length, ticker_type):
+    def _set_request_params(self, ticker, metric, time_length, start_date, end_date, ticker_type):
         if ticker_type == "indicator":
             security_str = "type:indicator,id:I:{},include:true".format(ticker)
         else:
             security_str = "id:{},include:true".format(ticker)
+        if start_date or end_date:
+            time_window = [
+                ("zoom", "custom"),
+                ("startDate", start_date if start_date else ""),
+                ("endDate", end_date if end_date else "")
+            ]
+        else:
+            time_window = [("zoom", time_length)]
         return [
             ("securities", security_str),
             ("calcs", "id:{},include:true".format(metric)),
-            ("zoom", time_length)
-        ]
+        ] + time_window
 
     def _validate_response(self, response):
         if response.status_code > 302:
@@ -47,27 +73,29 @@ class YChartsClient(object):
         )
         self._validate_response(response)
 
-    def get_security_prices(self, ticker, time_length):
+    def get_security_prices(self, ticker, time_length=None, start_date=None, end_date=None):
         """
         Get all price data for a ticker
 
         :param ticker: GOOG/AAPL/etc..
         :param time_length: 1 year (1)/3 months (3m)/etc..
         """
+        _validate_date_input(time_length, start_date, end_date)
         response = self._make_request(ticker, "price", time_length, "security")
         return self._get_raw_data(response)
 
-    def get_indicator_prices(self, ticker, time_length):
+    def get_indicator_prices(self, ticker, time_length=None, start_date=None, end_date=None):
         """
         Get all price data for an indicator
 
         :param ticker: JPIIP/USRSG/etc...
         :param time_length: 1 year (1)/3 months (3m)/etc..
         """
+        _validate_date_input(time_length, start_date, end_date)
         response = self._make_request(ticker, "price", time_length, "indicator")
         return self._get_raw_data(response)
 
-    def get_security_metric(self, ticker, metric, time_length):
+    def get_security_metric(self, ticker, metric, time_length=None, start_date=None, end_date=None):
         """
         Get all data for a given security metric
 
@@ -77,5 +105,8 @@ class YChartsClient(object):
         """
         if metric not in AVAILABLE_SECURITY_METRICS:
             raise Exception("{} is not in the list of available security metrics.".format(metric))
-        response = self._make_request(ticker, metric, time_length, "security")
+        _validate_date_input(time_length, start_date, end_date)
+        response = self._make_request(
+            ticker, metric, time_length, start_date, end_date, "security"
+        )
         return self._get_raw_data(response)
--- ychartspy/tests/test_client.py
+++ ychartspy/tests/test_client.py
@@ -7,12 +7,19 @@ from ychartspy.constants import YCHARTS_DATA_URL
 RAW_DATA = [1, 2, 3]
 
 
-def _make_security_params(ticker, metric):
+def _make_security_params(ticker, metric, time_length="1", start_date=None, end_date=None):
+    if start_date or end_date:
+        time_window = [
+            ("zoom", "custom"),
+            ("startDate", start_date if start_date else ""),
+            ("endDate", end_date if end_date else "")
+        ]
+    else:
+        time_window = [("zoom", time_length)]
     return [
         ("securities", "id:{},include:true".format(ticker)),
         ("calcs", "id:{},include:true".format(metric)),
-        ("zoom", "1")
-    ]
+    ] + time_window
 
 
 class MockResponse(object):
@@ -36,7 +43,7 @@ class TestYChartsClient(object):
 
     def test_make_request_with_security(self):
         self.client.session.get.return_value = MockResponse()
-        self.client._make_request("SPY", "price", "1", "security")
+        self.client._make_request("SPY", "price", "1", None, None, "security")
         eq_(self.client.session.get.call_count, 2)
         self.client.session.get.assert_called_with(
             YCHARTS_DATA_URL, params=_make_security_params("SPY", "price")
@@ -44,7 +51,7 @@ class TestYChartsClient(object):
 
     def test_make_request_with_indicator(self):
         self.client.session.get.return_value = MockResponse()
-        self.client._make_request("USRSG", "price", "1", "indicator")
+        self.client._make_request("USRSG", "price", "1", None, None, "indicator")
         eq_(self.client.session.get.call_count, 2)
         self.client.session.get.assert_called_with(YCHARTS_DATA_URL, params=[
             ("securities", "type:indicator,id:I:USRSG,include:true"),
@@ -74,3 +81,51 @@ class TestYChartsClient(object):
     def test_get_raw_data(self):
         data = self.client._get_raw_data(MockResponse())
         eq_(data, RAW_DATA)
+
+    def test_get_security_metric_with_start_date(self):
+        self.client.session.get.return_value = MockResponse()
+        response = self.client.get_security_metric("AAPL", "pe_ratio", start_date="01/01/2014")
+        eq_(self.client.session.get.call_count, 2)
+        self.client.session.get.assert_called_with(
+            YCHARTS_DATA_URL,
+            params=_make_security_params("AAPL", "pe_ratio", start_date="01/01/2014")
+        )
+        eq_(response, RAW_DATA)
+
+    def test_get_security_metric_with_end_date(self):
+        self.client.session.get.return_value = MockResponse()
+        response = self.client.get_security_metric("aapl", "pe_ratio", end_date="01/01/2014")
+        eq_(self.client.session.get.call_count, 2)
+        self.client.session.get.assert_called_with(
+            YCHARTS_DATA_URL,
+            params=_make_security_params("aapl", "pe_ratio", end_date="01/01/2014")
+        )
+        eq_(response, RAW_DATA)
+
+    def test_get_security_metric_with_start_and_end_date(self):
+        self.client.session.get.return_value = MockResponse()
+        response = self.client.get_security_metric(
+            "aapl", "pe_ratio", end_date="01/01/2014", start_date="01/01/2013"
+        )
+        eq_(self.client.session.get.call_count, 2)
+        self.client.session.get.assert_called_with(
+            YCHARTS_DATA_URL,
+            params=_make_security_params(
+                "aapl", "pe_ratio", end_date="01/01/2014", start_date="01/01/2013"
+            )
+        )
+        eq_(response, RAW_DATA)
+
+    @raises(Exception)
+    def test_get_security_metric_raises_exception_for_invalid_start_date(self):
+        self.client.session.get.return_value = MockResponse()
+        self.client.get_security_metric(
+            "aapl", "pe_ratio", end_date="01/01/2014", start_date="01-01-2013"
+        )
+
+    @raises(Exception)
+    def test_get_security_metric_raises_exception_for_invalid_end_date(self):
+        self.client.session.get.return_value = MockResponse()
+        self.client.get_security_metric(
+            "aapl", "pe_ratio", end_date="2014/01/01", start_date="01/01/2013"
+        )
