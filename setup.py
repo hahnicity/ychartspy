7af01803a59a4e427904ec998686673cd623fedb
hahnicity <grehm87@gmail.com>
2014-10-20 22:45:23 -0700

    Finalizing changes for v.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author="Gregory Rehm",
     version=__version__,
     description="Python Client for YCharts",
-    packages=find_packages(),
+    packages=find_packages(exclude=["*.tests"]),
     package_data={"*": ["*.html"]},
     install_requires=[
         "requests",
